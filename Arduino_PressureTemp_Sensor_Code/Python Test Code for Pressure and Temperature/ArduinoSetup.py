1f80539186661474fa968c85ec2f7379e6b6ca24
Awbmilne <Awbmilne@users.noreply.github.com>
2019-11-05 13:10:34 -0500

    Add to Pres_and_Temp_Code

---
+++ Arduino_PressureTemp_Sensor_Code/Python Test Code for Pressure and Temperature/ArduinoSetup.py
@@ -0,0 +1,104 @@
+# 
+# This library is created as an API between the Python
+# script and the Arduinos for the WatLock Design project
+#
+
+
+import time
+import serial
+import serial.tools.list_ports
+
+
+Arduino_List = [] # [English Name, Serial #, Port]
+
+###################################################################
+def Match_Arduinos ():
+    ports = list(serial.tools.list_ports.comports())                # Creates list of Available Serial Ports on the System
+    print("\n------List of Serial Ports Found------")               # Formatting Text
+    for p in ports:                                                 # For every available port
+        print (str(p) + " | " + p.hwid)                             # Print the Name and the port location
+        for a in Arduino_List:                                      # For every Arduino Specified in the main code
+            if a[1] in p.hwid:                                      # Check of the Serial Number matches the Serial Number of the Port
+                a[2] = p.device                                     # If it does, Set the 3rd value of the Serial Port List array to the Port location
+                print ("Port " + p.device + " is " + a[0] + "\n")   # Print that it was found and where it was found
+    
+    print("------Matches Made------")                               # List valid Matches
+    for a in Arduino_List:
+        if a[2] != "":
+            print(a[0]+ " | " + a[2])
+            
+    print("\n------Matches NOT Made------")                         # List Matches not made
+    for a in Arduino_List:
+        if a[2] == "":
+            print(a[0] + " was not found")
+            
+    print("-----------------------------")                          # Terminal output formatting
+###################################################################         
+def list_available_ports():                                         # A setup tool used to show the Name, Location, and Information of the available Serial Ports (usefull for getting the Serial Number of an Arduino)
+    ports = list(serial.tools.list_ports.comports())
+    print("\n------List of Serial Ports Found------")
+    for p in ports:
+        print (str(p) + " | " + p.hwid + " | " + p.device)       
+###################################################################  
+def Start_Serial(port):                                             # Start-Up a serial interface
+    
+    port.dtr = False                                                # Force Disable the Port
+    time.sleep(0.022)                                               # Pause (VERY IMPORTANT)
+    port.dtr = True                                                 # Force re-enable the Port (Forcing the port to restart allows us to not reboot the arduino every time we re-connect)
+
+    
+    while (True):
+        serial = port.readline()                                    # Continue to read the output of the Serial Port,
+        print(serial)
+        if (str(serial).find("Serial Connection is Ready")):        # Until the Arduino says it is ready to Communicate
+            break                                                   # At which point, Stop reading the Serial Port
+
+###################################################################
+class Create_Serial(serial.Serial):                                 # Create Serial Object with custom Methods (Custom)
+
+
+    def get_data(self):         
+        self.write(b'0')                                            # Write a '0' bit to the Arduino to say "Send me data"
+        start_time = time.time()                                    # Set timer for wait amount
+        wait_max = 0.5                                              # Wait up to 0.5 Seconds for arduino to reply (CHANGE THIS VALUE WITH CARE)
+        while(True):
+            if(self.inWaiting()>0):                                 # Wait for Arduino to post to Serial Port
+                data = str(self.readline())                         # Read reply as String
+                return(data)                                        # Return the data
+            if(time.time() >= start_time + wait_max):
+                return("ERROR: TIMEOUT")                            # Returns TIMEOUT if arduino didn't reply in time
+
+                
+    def send_command(self, command):            
+        self.write(b'1')                                            # Write a '1' bit to tell the Arduino "I want to send a Command"
+        time.sleep(0.022)           
+        if(self.inWaiting()>0):                                     # Wait for Arduino to post to serial
+            verification = self.readline()                          # Read the Serial line 
+            #print (verification)
+            if (str(verification).find("OK")):                      # Check that "OK" verification is in the String reply from Arduino.
+                for i in range(0,len(str(command))):                # For the length of the Command String
+                    self.write(str(command)[i].encode())            # Write the i'th character of the String to the Serial port
+                self.write(b'~')                                    # Once the entire string has been written, Write a '~' to the Serial Port as an end-command character (can use any Character, Just change in Arduino code)
+                start_time = time.time()                            # Set timer for wait amount
+                wait_max = 0.5                                      # Wait up to 0.5 Seconds for arduino to reply (CHANGE THIS VALUE WITH CARE)           
+                while(True):
+                    if(self.inWaiting()>0):                         # Check if Arduino Wrote anything       
+                        data = str(self.readline())                 # Read the Data Arduino Wrote
+                        return(data)
+                    if(time.time() >= start_time + wait_max):       # If wait_max exceeded
+                        return("ERROR: TIMEOUT")                    # Return "TIMEOUT" instead of the data (This allows the program to move on if an arduino has issues)
+
+                            
+    def other_command(self):            
+        self.write(b"2")                                            # Third command
+        start_time = time.time()
+        wait_max = 0.5
+        while(True):
+            if(self.inWaiting()>0):                                 # Wait for Arduino to post to Serial Port
+                reply = str(self.readline())                        # Read reply as String
+                return(reply)                                       # Return the reply from Arduino
+            if(time.time() >= start_time + wait_max):
+                return("ERROR: TIMEOUT")
+
+#if __name__ == '__main__':
+#    match_Arduinos()
---
+++ Arduino_PressureTemp_Sensor_Code/Python Test Code for Pressure and Temperature/Arduino_to_Python_Press_Temp.py
@@ -0,0 +1,84 @@
+# 
+# This program is a basic outline of interaction between
+# a Python Script and an Arduino.
+# 
+# The Basics of this Protocol can be expanded upon by
+# setting up seperate serial ports for each Arduino and 
+# addressing them each sequentially.
+# 
+
+import time                                                             # Needed for pauses in testing
+import ArduinoSetup as Arduino                                          # API Library for WatLock Arduino Commands
+
+
+# LIST OF ARDUINOS
+#Arduino.Arduino_List.append(["SERIAL_PORT_NAME"    , "SERIAL_#_OF_ARDUINO"     , ""])
+Arduino.Arduino_List.append (["Testduino"           , "85735313033351409161"    , ""]) 
+ 
+
+#NECCESARY SETUP FOR SERIAL OBJECTS
+Arduino.Match_Arduinos()                                                # Locate Arduinos based on Serial Number
+i = 0
+for a in Arduino.Arduino_List:
+    if Arduino.Arduino_List[i][2] != "":                                # if Match_Arduino found the arduino
+        exec (a[0] + " = Arduino.Create_Serial('" + a[2] + "', 9600)")  # Create Serial Objects for each Arduino in the list
+    i += 1
+i = 0
+for a in Arduino.Arduino_List:
+    if Arduino.Arduino_List[i][2] != "":                                # if Match_Arduino found the arduino
+        print(f"---Initializing {a[0]}")
+        exec ("Arduino.Start_Serial(" + a[0] + ")")                     # Start Each Serial Interface
+    i += 1
+
+print ("\n")
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#MAIN COMMAND LOOP
+
+# This loop just repeatedly sends 3 different commands,
+# the First being a data request, where the arduino just sends back data
+# the Second being a command, where the arduino must receive a string command and then reply
+# the Third being another command, could serve any purpose (possibly return the arduino information)
+
+while (True): #Run Forever
+
+    # Data request Command
+    if (Arduino.Arduino_List[0][2] != ''):
+        data_from_arduino = Testduino.get_data()                            # VSCode and other IDE's may dislike the use of an object that has not been created,
+        print (data_from_arduino)                                           # This is due to the dynamic creation of the Serial Objects in the code above. Ignore these errors.
+    else:
+        print ("Testduino Is not connected, Failed to get data")
+    
+    
+    # Command to send String Command
+    if (Arduino.Arduino_List[0][2] != ''):
+        reply_from_arduino = Testduino.send_command("Test Command")
+        print (reply_from_arduino)
+    else:
+        print ("Testduino Is not connected, Failed to send Command")
+
+    # Get Raw Pressure Data
+    if (Arduino.Arduino_List[0][2] != ''):
+        reply_from_arduino = Testduino.send_command("Raw Pressure")
+        print (reply_from_arduino)
+    else:
+        print ("Testduino Is not connected, Failed to send Command")
+    
+    # Get Raw Temperature Data
+    if (Arduino.Arduino_List[0][2] != ''):
+        reply_from_arduino = Testduino.send_command("Raw Temperature Celsius")
+        print (reply_from_arduino)
+    else:
+        print ("Testduino Is not connected, Failed to send Command")
+                
+
+    #Extra third command for example
+    if (Arduino.Arduino_List[0][2] != ''):
+        reply_from_other = Testduino.other_command()
+        print (reply_from_other)
+    else:
+        print ("Testduino Is not connected, Failed to send Command")
+
+    print ("\n")
+    #time.sleep()                                                      # Pause so the terminal doesnt fill instantly (only needed for testing)
+#---------------------------------------------------------------------------------------------------------------------------------   
