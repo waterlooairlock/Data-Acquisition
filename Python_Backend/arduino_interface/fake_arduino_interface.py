d54a08814c3f580984f7c44c9697815eb4c624d1
Austin Milne <awbmilne@gmail.com>
2020-12-20 15:27:33 -0500

    Add Fake Arduino for Backend Testing

---
+++ Python_Backend/arduino_interface/fake_arduino_interface.py
@@ -0,0 +1,33 @@
+
+import time
+
+import random
+import logging
+from custom_logging import log_path, get_logger
+
+random.seed("WatLock")
+
+class interface:
+    def __init__(self):
+        # Configure Logging for Fake Arduino
+        log_format = '%(asctime)s  %(name)18s : %(levelname)7s > %(message)-70s [%(filename)-21s | ln:%(lineno)d]'
+        file_handler = logging.FileHandler(filename=log_path+'Fake_Arduino.log', mode='w')
+        file_handler.setFormatter(logging.Formatter(log_format))
+        file_handler.setLevel(logging.DEBUG) # NOTE <-- Set log level here, Generally set to DEBUG
+        self.logger = get_logger("Fake Arduino")
+        self.logger.addHandler(file_handler)
+        # Log Initialization
+        self.logger.info("Initializing Fake Arduino")
+        pass
+
+    def get_sensor_reading(self, arduino_ID: int, sensor_number: int):
+        # Generate Random float value
+        value = random.uniform(0, 10)
+        # Log Request
+        self.logger.info("Data request: {arduino_ID:%s, sensor_number:%s}, returned value {%s}", arduino_ID, sensor_number, value)
+        return value
+
+    def send_command(self, arduino_ID: int, command_code: int, extra_data: bytearray):
+        # Log Command
+        self.logger.info("Command: {arduino_ID:%s, command_code:%s, extra_data:%s", arduino_ID, command_code, extra_data)
+        return
